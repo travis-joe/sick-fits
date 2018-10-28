5300a95530254fcac3a55b86fb241da4763b60c8
qzy <weelgod@gmail.com>
2018-10-28 16:08:06 +0800

    1-4 Introduction and Setup

---
+++ frontend/components/Meta.js
@@ -0,0 +1,13 @@
+import Head from "next/head";
+
+const Meta = () => (
+  <Head>
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta charSet="utf-8" />
+    <link href="/static/favicon.png" rel="shortcut icon"/>
+    <link href="/static/npgrogress.css" rel="stylesheet" type="text/css"/>
+    <title>Sick Fits!</title>
+  </Head>
+);
+
+export default Meta;
